3b12b7379925bacad4d7b1fca347465b883647ff
Yamin Satsuki <yamada.osaru@gmail.com>
2024-07-04 23:37:34 +0900

    Move Zone class to file

--- ValueObjects/Field.cs
+++ ValueObjects/Field.cs
@@ -36,38 +36,6 @@ public class Field
     }
 }
 
-public class Zone
-{
-    public List<Card> Cards { get; set; } = [];
-
-    public void Put(Card card)
-    {
-        Cards.Add(card);
-    }
-
-    public void Push(Card card)
-    {
-        Cards.Insert(0, card);
-    }
-
-    public Card Pop()
-    {
-        if (Cards.Count == 0)
-        {
-            throw new IndexOutOfRangeException();
-        }
-
-        var card = Cards[0];
-        Cards.RemoveAt(0);
-        return card;
-    }
-
-    public bool CardExists()
-    {
-        return Cards.Count > 0;
-    }
-}
-
 public enum ZoneType
 {
     MonsterZoneLeft,
---
+++ ValueObjects/Zone.cs
@@ -0,0 +1,33 @@
+﻿namespace YuGiOh.ValueObjects;
+
+public class Zone
+{
+    public List<Card> Cards { get; set; } = [];
+
+    public void Put(Card card)
+    {
+        Cards.Add(card);
+    }
+
+    public void Push(Card card)
+    {
+        Cards.Insert(0, card);
+    }
+
+    public Card Pop()
+    {
+        if (Cards.Count == 0)
+        {
+            throw new IndexOutOfRangeException();
+        }
+
+        var card = Cards[0];
+        Cards.RemoveAt(0);
+        return card;
+    }
+
+    public bool CardExists()
+    {
+        return Cards.Count > 0;
+    }
+}
